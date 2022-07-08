4d4a0061711033dc9598c62935a187d154c27b8f
AtAtanasov2022 <103512175+AtAtanasov2022@users.noreply.github.com>
2022-07-08 14:23:14 +0300

    UPDATE

--- online-shop2/src/App.vue
+++ online-shop2/src/App.vue
@@ -1,38 +1,20 @@
 <template>
-  <div>
-    <nav>
-      <router-link to="/login">Login</router-link> |
-      <router-link to="/registration">Registration</router-link> |
-      <router-link to="/">Home</router-link> |
-    </nav>
-    <router-view></router-view>
-  </div>
+  <v-app>
+    
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-nav {
-  padding: 30px;
-  background-color: rgb(18, 115, 218);
-  border: 1px;
-  border-radius: 40%;
-  width: 60%;
-  margin-left: 17%;
+export default {
+  name: 'App',
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+  data: () => ({
+    //
+  }),
+};
+</script>
--- online-shop2/src/views/HomeView.vue
+++ online-shop2/src/views/HomeView.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HomePage msg="Welcome to Your Vue.js App"></HomePage> 
-  </div>
+  <HomePage></HomePage>
 </template>
 
 <script>
-// @ is an alias to /src
-import HomePage from '@/components/HomePage.vue'
+  import HomePage from '../components/HomePage'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HomePage
+  export default {
+    name: 'HomeView',
+
+    components: {
+      HomePage,
+    },
   }
-}
 </script>
