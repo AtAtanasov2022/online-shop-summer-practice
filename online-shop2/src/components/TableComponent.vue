fc291cbc5bd307109f6b63a5ceffb2351b73545b
AtAtanasov2022 <103512175+AtAtanasov2022@users.noreply.github.com>
2022-08-05 11:18:41 +0300

    UPDATE Added filters and changed designs

--- online-shop2/src/components/TableComponent.vue
+++ online-shop2/src/components/TableComponent.vue
@@ -1,20 +1,43 @@
 <template>
-  <v-data-table
-    v-if="posts"
-    :headers="headers"
-    :items="filteredPosts"
-    :items-per-page="5"
-    item-key="name"
-    class="elevation-1"
-    :footer-props="{
-      showFirstLastPage: true,
-      firstIcon: 'mdi-arrow-collapse-left',
-      lastIcon: 'mdi-arrow-collapse-right',
-      prevIcon: 'mdi-minus',
-      nextIcon: 'mdi-plus',
-    }"
-    >Hello</v-data-table
-  >
+  <div>
+    <div class="filters">
+      <v-select
+        class="filter"
+        v-model="filterInput1"
+        :items="names"
+        chips
+        label="Names"
+        multiple
+        outlined
+      ></v-select>
+      <v-select
+        class="filter2"
+        v-model="filterInput2"
+        :items="dates"
+        chips
+        label="Created at"
+        multiple
+        outlined
+      ></v-select>
+    </div>
+
+    <v-data-table
+      v-if="posts"
+      :headers="headers"
+      :items="filteredPosts"
+      :items-per-page="5"
+      item-key="name"
+      class="elevation-1"
+      :footer-props="{
+        showFirstLastPage: true,
+        firstIcon: 'mdi-arrow-collapse-left',
+        lastIcon: 'mdi-arrow-collapse-right',
+        prevIcon: 'mdi-minus',
+        nextIcon: 'mdi-plus',
+      }"
+      >Hello</v-data-table
+    >
+  </div>
 </template>
 
 <script>
@@ -46,6 +69,10 @@ export default {
           value: "authorUsername",
         },
       ],
+      names: [],
+      dates: [],
+      filterInput1: "",
+      filterInput2: "",
     };
   },
 
@@ -57,6 +84,14 @@ export default {
     ...mapState({
       posts: (state) => state.allPosts2,
     }),
+
+    filterInput1change() {
+      return this.filterInput1;
+    },
+
+    filterInput2change() {
+      return this.filterInput2;
+    },
   },
 
   beforeMount() {
@@ -72,6 +107,8 @@ export default {
       };
       id++;
       this.filteredPosts.push(temporartObject);
+      this.names.push(element.author.username);
+      this.dates.push(element.created_at);
     });
 
     // console.log(this.filteredPosts);
@@ -82,22 +119,69 @@ export default {
 </script>
 
 <style>
-@media (max-width: 360px) and (max-height: 740px) {
-  .v-data-table > .v-data-table__wrapper .v-data-table__mobile-table-row {
-    border-top: thin solid !important;
-    display: initial;
-  }
+.v-data-table > .v-data-table__wrapper .v-data-table__mobile-table-row {
+  border-top: thin solid !important;
+  display: initial;
+}
 
+.theme--light.v-data-table {
+  width: 95%;
+  margin: auto;
+  margin-top: -5px;
+  border: thin solid;
+  border-radius: 10px;
+}
+
+.theme--light.v-data-table .v-data-footer {
+  border-top: thin solid;
+}
+
+.filters {
+  display: flex;
+  align-items: center;
+  align-content: center;
+  justify-content: space-between;
+  flex-direction: row;
+  padding: 10px;
+}
+
+@media (max-width: 360px) and (max-height: 740px) {
   .theme--light.v-data-table {
     width: 95%;
     margin: auto;
-    margin-top: 10px;
+    margin-top: -5px;
     border: thin solid;
     border-radius: 10px;
   }
-  
+
   .theme--light.v-data-table .v-data-footer {
     border-top: thin solid !important;
+  }
+
+  .filter {
+    width: 40%;
+  }
+
+  .filter2 {
+    width: 40%;
+    margin-left: 6px !important;
+  }
+
+  .filters {
+    margin-top: 5px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    align-content: center;
+    justify-content: space-between;
+    padding: 10px;
+  }
+
+  .v-text-field.v-text-field--enclosed:not(.v-text-field--rounded)
+    > .v-input__control
+    > .v-input__slot,
+  .v-text-field.v-text-field--enclosed .v-text-field__details {
+    height: 10px !important;
   }
 }
 </style>
